d12bf38e9dbe9f12b50df83744b4c49b0abc9224
Thanh Dung Nguyen <dung.nguyenthanh@hotmail.com>
2023-03-19 19:04:26 -0500

    Big Data - Spark Scala Project - Trying step 5b

--- BigData/Projects_Spark_Scala/mtcars.scala
+++ BigData/Projects_Spark_Scala/mtcars.scala
@@ -6,12 +6,15 @@ val header = data.first()
 val filteredData = data.filter(line => line != header)
 // Split the data by commas
 val splitData = filteredData.map(line => line.split(","))
+
+// Step 2. Select a categorical variable and a numeric variable and form the key-value pair and create a pairRDD called “population”.
 // Extract the columns and convert numeric variable
 val catNumRDD = splitData.map(columns => (columns(2).toInt, columns(1).toDouble)) 
 val population = sc.parallelize(catNumRDD.collect())
 // Print content
 population.foreach(println)
 
+// Step 3. Compute the mean mpg and variance for each category and display
 // Group by the categorical variable
 val groupedRDD = catNumRDD.groupByKey()
 // Compute the mean and variance for each category
@@ -23,7 +26,26 @@ val statsRDD = groupedRDD.mapValues(data => {
 // Print the contents of the statsRDD RDD
 statsRDD.foreach(println)
 
-// Create a bootstrap sample by randomly sampling 25% of the population without replacement
+// Step 4. Create the sample for bootstrapping. All you need to do is take 25% of the population without replacement.
 val sample = population.sample(false, 0.25)
 // Print the contents of the sample RDD
 sample.foreach(println)
+
+// 5a. Create a "resampledData". All you need to do is take 100% of the sample with replacement.
+println("resampledData:")
+val resampledData = sample.takeSample(true, 1)
+
+// 5b. Compute the mean mpg and variance for each category
+// Making RDD from parallelized collections:
+val resampledDataRDD = sc.parallelize(resampledData)
+// Group by the categorical variable
+val resampledGroupedRDD = resampledDataRDD.groupByKey()
+// Compute the mean and variance for each category
+val resampledStatsRDD = resampledGroupedRDD.mapValues(data => {
+  val mean = data.sum / data.size
+  val variance = data.map(x => math.pow(x - mean, 2)).sum / data.size
+  (mean, variance)
+})
+// Print the contents of the statsRDD RDD
+println("resampledStatsRDD:")
+resampledStatsRDD.foreach(println)
